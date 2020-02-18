83aa5f0873c01878ce9ef7152e9e7b470a36c464
erwin <erwin.chang@titansoft.com.sg>
2020-02-19 02:45:58 +0800

    init proj

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CnRedirect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string android = "0")
+        {
+            if (android == "1" && Request.Headers["User-Agent"].ToString().ToLower().Contains("micromessenger"))
+            {
+                Response.Headers.Add("Content-Disposition", "attachment; filename='openDefaultBrowser.pdf'");
+                Response.ContentType = "application/pdf";
+            }
+
+            ViewBag.TargetUrl = "https://play.99168.fun";
+
+            return View();
+        }
+    }
+}
